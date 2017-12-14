100311696fbb353481f5d8f3f8f55f69c50ee0fb
nine <nine@segmentfault.com>
2017-12-14 16:24:05 +0800

    log accessor

--- src/Common/Accessor/Log.php
+++ src/Common/Accessor/Log.php
@@ -7,12 +7,19 @@
 
 namespace MultiProcess\Common\Accessor;
 
+use MultiProcess\Common\Facades\File;
+use MultiProcess\Common\Facades\Config;
+
 /**
  * Class Log
  * @package MultiProcess\Common\Accessor
  */
 class Log
 {
+    /**
+     * 默认内容类型
+     */
+    const CONTENT_FORMAT = 'Y-m-d H:i:s';
 
     /**
      * @var
@@ -27,6 +34,35 @@ class Log
 
     protected function outputToFile($method)
     {
+        $prefix = "[" . date(Config::get('log.content_format', self::CONTENT_FORMAT)) . "] " . Config::get('init
+        .env') . '.' . $method . ':';
+
+        $this->console($prefix);
+    }
+
+    /**
+     * @return null|string
+     */
+    protected function parseParams()
+    {
+        if (is_array($this->params)) {
+            return json_encode($this->params);
+        } elseif (is_string($this->params)) {
+            return $this->params;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param string $prefix
+     */
+    protected function console($prefix = '')
+    {
+        $logFile = File::getLogFile();
+        $params = $this->parseParams();
+
+        file_put_contents($logFile, $prefix . $params, FILE_APPEND);
 
     }
 
